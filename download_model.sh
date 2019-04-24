821b8a6e87cd88c40f335528baa5ff8bb46a9084
swxEmily <shiwenxiu@huawei.com>
2019-04-24 16:45:41 +0800

    Title:download model script Description:

--- download_model.sh
+++ download_model.sh
@@ -25,10 +25,6 @@ tools_version=$1
 
 function parse_download_branch()
 {
-    if [[ ${parse_download_branch_status}"X" != "X" ]];then
-        return 0
-    fi
-    
     echo "parse download_branch for ${tools_version}..."
     rm -rf ${script_path}/get_download_branch.sh
     rm -rf ${script_path}/branch_desc.txt
@@ -47,7 +43,7 @@ function parse_download_branch()
     else
         echo -e "\033[33mDownload models from special branch ${download_branch}, special branch definition in github:Ascend/models/branch_desc.txt for ${tools_version}\033[0m"
     fi
-    parse_download_branch_status="downloaded"
+
     return 0
 }
 
